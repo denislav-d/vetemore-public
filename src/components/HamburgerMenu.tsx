f876f4bfb12e3d3a5cefc23ba3a5ba99a89df001
denislav-d <ddenislav333@gmail.com>
2025-07-10 23:30:00 +0200

    Refactor HamburgerMenu component to improve variable naming and clarity
    - Rename pathVariants to svgPathVariants for better context.
    - Ensure consistent naming conventions for improved readability.

--- src/components/HamburgerMenu.tsx
+++ src/components/HamburgerMenu.tsx
@@ -4,12 +4,12 @@ import { hamburgerNavItems, navEssentials } from "@/data/content";
 
 import "@/styles/components/_hamburger-menu.scss";
 
-const pathVariants1 = {
+const svgPathVariants1 = {
   open: { d: "M3.06061 2.99999L21.0606 21" },
   closed: { d: "M0 9.5L24 9.5" },
 };
 
-const pathVariants2 = {
+const svgPathVariants2 = {
   open: { d: "M3.00006 21.0607L21 3.06064" },
   moving: { d: "M0 14.5L24 14.5" },
   closed: { d: "M0 14.5L24 14.5" },
@@ -17,6 +17,7 @@ const pathVariants2 = {
 
 export default function HamburgerMenu() {
   const [animation, setAnimation] = useState("closed");
+
   const onClick = () => {
     setAnimation("moving");
     setTimeout(() => {
@@ -67,13 +68,13 @@ export default function HamburgerMenu() {
             strokeWidth="1.75"
             stroke="#1A1313"
             animate={animation}
-            variants={pathVariants1}
+            variants={svgPathVariants1}
           />
           <motion.path
             strokeWidth="1.75"
             stroke="#1A1313"
             animate={animation}
-            variants={pathVariants2}
+            variants={svgPathVariants2}
           />
         </svg>
       </button>
